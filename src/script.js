555105836e3045ea5f67f799190283b0dbaf5b45
daniel heppenstiel <danieltristanheppenstiel@gmail.com>
2023-07-24 10:28:28 -0400

    Fourth Commit
    Light mode / Dark Mode Bug fixes / added functionality

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
+const itemCounterElement = document.querySelector('#items-left');
     // Buttons
 const allBtn = document.getElementById('all-btn');
 const activeBtn = document.getElementById('active-btn');
@@ -9,7 +10,8 @@ const completedBtn = document.getElementById('completed-btn');
     // Toggle Icons
 var sunIcon = document.getElementById('toggle-sun');
 var moonIcon = document.getElementById('toggle-moon');
-
+    // Current Mode
+let isDarkMode = true;
 
 // Functions
 function addToLocalStorage(key, item) {
@@ -52,18 +54,25 @@ function createTodo(todo) {
     li.appendChild(para);
     li.appendChild(img);
 
+    if(!isDarkMode) {
+        li.classList.add('light-mode-item');
+    };
+    
     return li;
 };
 
 function addTodo(e) {
     e.preventDefault();
+    const currentItems = todoList.childNodes;
     // create todo
     const newTodo = todoInput.value;
     const todo = createTodo(newTodo);
     // Add Todo to the DOM
+    console.log(currentItems);
     todoList.appendChild(todo);
     // add todo to local storage
-    addToLocalStorage('all', newTodo);
+    addToLocalStorage('active', newTodo);
+    todoInput.value = '';
 };
 
     // Toggle Display
@@ -82,6 +91,7 @@ function toggleDisplay() {
 
     // Update styles dark mode / light mode
     if (moonIcon.classList.contains('display-moon') && windowWidth < 500) { // light mode
+            isDarkMode = false;
             body.classList.add('light-mode');
             body.style.backgroundColor = 'hsl(236, 33%, 92%)';
             controls.classList.add('light-mode-item');
@@ -93,6 +103,7 @@ function toggleDisplay() {
             });
 
     } else if (moonIcon.classList.contains('display-moon') && windowWidth >= 500) {
+            isDarkMode = false;
             body.classList.add('light-mode-large');
             body.style.backgroundColor = 'hsl(236, 33%, 92%)';
             controls.classList.add('light-mode-item');
@@ -103,6 +114,7 @@ function toggleDisplay() {
             });
             
     } else { // Dark mode
+            isDarkMode = true;
             body.classList.remove('light-mode');
             body.classList.remove('light-mode-large');
             body.style.backgroundColor = 'hsl(235, 21%, 11%)';
@@ -118,9 +130,7 @@ function toggleDisplay() {
 };
 
 function interactTodo(e) {
-
     const target = e.target;
-
     if (target.classList.contains('remove-icon')) {
         target.parentElement.remove();
     } else if (target.classList.contains('circle')) {
@@ -132,10 +142,54 @@ function interactTodo(e) {
         }, 2000);
     } else {
         return;
-    }
+    };
+};
+
+
+    // Controls All Active Completed
+function displayCompleted() {
+    // clear the list
+    todoList.innerHTML = '';
+    // get todo's from local storage
+    const completedItems = JSON.parse(localStorage.getItem('completed'));
+    // loop through todos and add each one to the DOM
+    completedItems.forEach(item => {
+       const completedTodo = createTodo(item);
+       todoList.appendChild(completedTodo);
+    });
+
+    itemCounterElement.innerText = `${completedItems.length} Items remaining..`
+
 
 };
 
+function displayActive() {
+    todoList.innerHTML = '';
+    const activeItems = JSON.parse(localStorage.getItem('active'));
+    activeItems.forEach(item => {
+        const activeItems = createTodo(item);
+        todoList.appendChild(activeItems);
+    });
+
+    itemCounterElement.innerText = `${activeItems.length} Items remaining..`
+
+
+};
+
+function displayAll() {
+    todoList.innerHTML = '';
+    let allTodos = [];
+    const completedItems = JSON.parse(localStorage.getItem('completed'));
+    const activeItems = JSON.parse(localStorage.getItem('active'));
+    allTodos = [...activeItems, ...completedItems];
+    allTodos.forEach(todo => {
+        const allItems = createTodo(todo);
+        todoList.appendChild(allItems);
+    });
+
+    itemCounterElement.innerText = `${allTodos.length} Items remaining..`
+
+};
 
 
 // Event Listeners
@@ -144,10 +198,11 @@ sunIcon.addEventListener('click', toggleDisplay);
 moonIcon.addEventListener('click', toggleDisplay);
 
 todoList.addEventListener('click', interactTodo);
+completedBtn.addEventListener('click', displayCompleted);
+activeBtn.addEventListener('click', displayActive);
+allBtn.addEventListener('click', displayAll);
 
 
 
-// LOCAL STORAGE FUNCTIONALITY
-    // 1.set items to local storage based on their keu (all, active, completed)
-    // 2. when all, active or completed is clicked, I want the corresponding todo's to be displayed
-    // 3. Be able to remove items from local storage
+// Check if current items are in light mode or dark mode
+// add new item based on current mode
